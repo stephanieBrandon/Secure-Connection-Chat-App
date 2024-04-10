5e8883a5a03bcb665edf6edb5551d595bbcaef0e
stephBrandon <stephanieswebdesign@gmail.com>
2024-04-09 22:06:06 -0400

    altered code to cleaner methods. Still needs username label to work.

--- chatBox.py
+++ chatBox.py
@@ -6,9 +6,7 @@ from kivy.uix.label import Label
 
 #imports for connecting to server 
 import socket 
-from threading import Thread
-"""import sys
-print(sys.path)"""
+import threading
 from client import MySocket
 
 
@@ -19,16 +17,38 @@ class ChatApp(App):
         # connect to the server setting set in file chat-server
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('localhost', 1077))
-        #threading.Thread(target=self.receive_messages).start()
-    """def handle_received_message(self, message):
-        # Process the message (e.g., extract sender, timestamp, etc.)
-        # For now, let's assume the message format is "<sender>: <message>"
-        _, received_message = message.split(":", 1)
-        self.update_message(received_message.strip())  # Update the chat label
-"""
-    #- sb !important
-    def recieved_data(self, instance):
-            self.incoming_msg_label.text = self.client_socket.recv(1024).decode()
+        # -sb -Tues
+        # -??? self.client_socket.send('clientA'.encode('utf-8'))
+    def connect_to_server(self):
+        #connect method to connect our client to the server.
+        self.client_socket.connect((self.host, self.port))
+        #where 'client' is we want the entry of what user put in as their user name
+        # - yulia replace name_text with kivy version
+        username = self.username_input.get()
+        self.client_socket.send(username.encode('utf-8'))
+        #self.name_label['state'] = "disable"
+        self.username_input['state'] = "disable"
+        self.connect_button['state'] = "disable"
+        msg_thread = threading.Thread(target=self.listen_to_server)
+        msg_thread.daemon = True # marked as Daemon so that it will close when the main thread closes.
+        msg_thread.start()
+    
+    def listen_to_server(self):
+        while (True):
+            dataBytes = self.client_socket.recv(1024)
+            #yulia replace with text box 
+            self.incoming_msg_label.text =  dataBytes.decode()
+
+    def send_to_server(self):
+        #send the message from client to the server.
+        #yulia -substitute in kivy? -- shows users msgs in blue
+       #self.main_textbox.tag_config("self_message", foreground="blue")
+        message = self.text_input.get()
+        #encode to convert the string to array of bytes to send to the server
+        self.client_socket.send(message.encode('utf-8'))
+        #clearing the input box
+        self.text_input.text = ''
+        self.incoming_msg_label.text = f'\n{self.name_text.get()}:\n   {message}' #, "self_message" add back if able to create blue in kivy
 
     def build(self):
         layout = BoxLayout(orientation='vertical')
@@ -47,14 +67,6 @@ class ChatApp(App):
             def __init__(self, **kwargs):
                 super(MyLabel, self).__init(**kwargs)
 
-                self.sock = MySocket()
-                Thread(target=self.get_data).start()
-
-                def get_data(self):
-                    while True:
-                     recieved_msg = 'test'
-                     recieved_msg = self.text = self.sock.get_data()
-        
         # space to type
         self.text_input = TextInput(hint_text='Type here', multiline=False)
         layout.add_widget(self.text_input)
@@ -62,18 +74,27 @@ class ChatApp(App):
         # send button
         button = Button(text='Send')
         #connect to function
-        button.bind(on_press=self.on_button_click)
+        button.bind(on_press=self.on_button_click) #-sb remove?
+        button.bind(on_press=self.send_to_server)
         #- sb !important
-        button.bind(on_release=self.recieved_data)
+        #button.bind(on_release=self.recieved_data)
         layout.add_widget(button)
+
+        # space to type
+        self.username_input = TextInput(hint_text='Username', multiline=False)
+        layout.add_widget(self.username_input)
+        #connect to server button
+        connect_button = Button(text='Connect')
+        connect_button.bind(on_press=self.connect_to_server)
 
         return layout
 
     #function to reflect typed messages use for testing
     def on_button_click(self, instance):
-        message = self.text_input.text
+
+       # message = self.text_input.text
         #send message to server
-        self.client_socket.send(message.encode('utf-8'))
+      #  self.client_socket.send(message.encode('utf-8'))
      # clear input box 
         self.text_input.text = '' 
 
--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-# -sb this is so that the GUI can get the messages back to display.
-class MySocket:
-    def __init__(self, host="localhost", port= 1077):
-        self.sock = socket.socket()
-        self.sock.connect((host, port))
-        def get_data(self):
-            return self.sock.recv(1024)
